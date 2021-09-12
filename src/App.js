b1ffa1359a9df23222ca3cc0b7c686a8f80766fa
Ranjan Paul <paulranjan694@gmail.com>
2021-09-12 15:28:57 +0530

    Breakout sign-in form into FormInput & CustomButton components

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 import Header from "./components/header/header.component";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/sign-in" component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
