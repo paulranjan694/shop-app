b4e835d3e96f93b777c6aa53bf40c98272325b20
Ranjan Paul <paulranjan694@gmail.com>
2021-09-16 20:24:13 +0530

    Added redux-persist library

--- src/index.js
+++ src/index.js
@@ -8,12 +8,15 @@ import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 
-import store from './redux/store';
+import {store, persistor} from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />  
+      <PersistGate persistor={persistor}>
+        <App />  
+      </PersistGate>
     </BrowserRouter>
   </Provider>,
   document.getElementById('root')
