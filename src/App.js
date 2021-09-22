bb8b0e099457b2368bfbab1c5cf8a630446af165
Ranjan Paul <paulranjan694@gmail.com>
2021-09-22 23:20:30 +0530

    implemented code splitting with lazy loading

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 import "./App.css";
 import { createStructuredSelector } from "reselect";
 
 import Header from "./components/header/header.component";
-import HomePage from "./pages/homepage/homepage.component";
-import ShopPage from "./pages/shop/shop.component";
-import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+// import HomePage from './pages/homepage/homepage.component'
+// import ShopPage from "./pages/shop/shop.component";
+// import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 
-import { addCollectionAndDocuments, auth, createUserProfileDocument } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 import { connect } from "react-redux";
 import { setCurrentUser } from "./redux/user/user.actions";
 import { selectCurrentUser } from "./redux/user/user.selectors";
-import CheckoutPage from "./pages/checkout/checkout.component";
+// import CheckoutPage from "./pages/checkout/checkout.component";
 import { selectCollectionsForPreview } from "./redux/shop/shop.selectors";
+
+const HomePage = lazy(() => import("./pages/homepage/homepage.component"));
+const ShopPage = lazy(() => import("./pages/shop/shop.component"));
+const SignInAndSignUpPage = lazy(() => import("./pages/sign-in-and-sign-up/sign-in-and-sign-up.component"));
+const CheckoutPage = lazy(() => import("./pages/checkout/checkout.component"));
 
 class App extends React.Component {
   unsubscribeFromAuth = null;
@@ -52,6 +57,7 @@ class App extends React.Component {
       <div>
         <Header />
         <Switch>
+          <Suspense fallback={<p>LOADING...</p>}>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
           <Route path="/checkout" component={CheckoutPage} />
@@ -65,6 +71,7 @@ class App extends React.Component {
               )
             }
           />
+          </Suspense> 
         </Switch>
       </div>
     );
